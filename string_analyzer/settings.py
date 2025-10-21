d5b1248ee778f74a971c637d75aba6fea4c790bc
Olaitan34 <emmfatsneh@gmail.com>
2025-10-21 04:11:46 +0100

    Disable CSRF for REST API endpoints

--- string_analyzer/settings.py
+++ string_analyzer/settings.py
@@ -156,6 +156,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
